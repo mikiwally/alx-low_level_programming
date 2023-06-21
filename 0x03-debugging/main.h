292f9da8defcf0a0ebdc50db67c5bb45f31e9589
Mikhail <mikilewis08@gmail.com>
2023-06-21 06:02:52 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -8,5 +8,4 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
-carrie@ubuntu:/debugging$ 
+#endif /* MAIN_H */ 
